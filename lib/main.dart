06dbea2e9531c7e95e941dcbff49a481ef655c0d
Hugo Rayhan Firmansyah <hugo.hgrayhan08@gmail.com>
2023-05-23 22:39:53 +0700

    menghapus file home_thread yg double

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:capstone_mobile/screen/home_thread_screen.dart';
+import 'package:capstone_mobile/screen/home_Thread/home_thread_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
