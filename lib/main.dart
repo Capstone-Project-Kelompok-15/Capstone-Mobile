42814ef800ebb318aefe6a3209c07c7b599d9bef
unknown <windyindriarnie@gmail.com>
2023-06-14 19:56:23 +0700

    benerinnnn

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:capstone_mobile/screen/bookmark/bookmark_screen.dart';
 import 'package:capstone_mobile/screen/home_Thread/create_thread_screen.dart';
 import 'package:capstone_mobile/screen/home_Thread/home_thread_screen.dart';
 import 'package:capstone_mobile/screen/home_Thread/komentar_screen.dart';
 import 'package:capstone_mobile/screen/home_buttomNavigasi_screen.dart';
+import 'package:capstone_mobile/screen/pemberitahuan/pemberitahuan_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,14 +17,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeButtonmNavigasiScreen(),
-      initialRoute: HomeButtonmNavigasiScreen.routename,
+      home: 
+      const HomeButtonmNavigasiScreen(),
+      // BookmarkScreen(),
+      initialRoute: 
+      HomeButtonmNavigasiScreen.routename,
+      // BookmarkScreen.routename,
       routes: {
         HomeButtonmNavigasiScreen.routename: (context) =>
             const HomeButtonmNavigasiScreen(),
         HomeThreadScreen.routename: (context) => HomeThreadScreen(),
         CreateThreadScreen.routename: (context) => const CreateThreadScreen(),
         KomentarScreen.routename: (context) => const KomentarScreen(),
+        PemberitahuanScreen.routename: (context) => const PemberitahuanScreen(),
+        BookmarkScreen.routename: (context) => BookmarkScreen(),
       },
     );
   }
