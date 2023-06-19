12b4cb79ba225ed57550f282bd99246a5414e4fa
ajengniarti <201111007@mhs.stiki.ac.id>
2023-06-20 01:31:02 +0700

    edit beberapa yang error

--- lib/screen/landing_pages/landing_screen.dart
+++ lib/screen/landing_pages/landing_screen.dart
@@ -1,83 +1,53 @@
-import 'package:capstone_mobile/screen/landing_pages/landing_screen_1.dart';
-import 'package:capstone_mobile/screen/landing_pages/landing_screen_3.dart';
+import 'package:capstone_mobile/style/font_style.dart';
+import 'package:capstone_mobile/widget/button.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-import '../../style/color_style.dart';
-import '../../style/font_style.dart';
-import 'landing_screen_2.dart';
+import '../login/login_screen.dart';
 
-class LandingScreen extends StatefulWidget {
-  static const routename = '/landing-screen';
-  const LandingScreen({super.key});
+class LandingScreen3 extends StatefulWidget {
+  const LandingScreen3({super.key});
 
   @override
-  State<LandingScreen> createState() => _LandingScreenState();
+  State<LandingScreen3> createState() => _LandingScreen3State();
 }
 
-class _LandingScreenState extends State<LandingScreen> {
-  final PageController _controller = PageController();
-  bool onLastPage = false;
-
+class _LandingScreen3State extends State<LandingScreen3> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: [
-      GestureDetector(
-        onTap: () {
-          _controller.nextPage(
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeIn);
-        },
-        child: PageView(
-          controller: _controller,
-          onPageChanged: (index) {
-            setState(() {
-              onLastPage = (index == 2);
-            });
-          },
-          children: const [
-            LandingScreen1(),
-            LandingScreen2(),
-            LandingScreen3()
-          ],
-        ),
-      ),
-      onLastPage
-          ? Container()
-          : Padding(
-              padding: const EdgeInsets.only(top: 54, right: 16),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  height: 37,
-                  width: 113,
-                  decoration: BoxDecoration(
-                      color: primary500,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: TextButton(
-                      onPressed: () {
-                        _controller.jumpToPage(2);
-                      },
-                      child: Text(
-                        "Lewati",
-                        style: buttonMedium.copyWith(color: typography0),
-                      )),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 505,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/landing_3.png'),
+                    fit: BoxFit.cover)),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Dapat Membentuk\nCommunity\nSecara Online",
+                  textAlign: TextAlign.center,
+                  style: extraLargeMedium,
                 ),
-              ),
+                const SizedBox(height: 74.4),
+                Button(
+                    buttonText: "Lanjutkan",
+                    width: 138,
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(LoginScreen.routename,);}
+                    ),
+                const SizedBox(height: 119,)
+              ],
             ),
-      Container(
-          alignment: const Alignment(0, 0.85),
-          child: SmoothPageIndicator(
-            effect: ExpandingDotsEffect(
-                activeDotColor: info500,
-                dotHeight: 4.16,
-                dotWidth: 16.64,
-                expansionFactor: 2,
-                dotColor: const Color(0xFF3A3A3C)),
-            count: 3,
-            controller: _controller,
-          ))
-    ]));
+          )
+        ],
+      ),
+    );
   }
 }
