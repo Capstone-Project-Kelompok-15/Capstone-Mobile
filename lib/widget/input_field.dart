6f032bb0cc8453377e4e4b3a8e8b653d1bb44458
khairaanr <khairahardja16@gmail.com>
2023-06-26 11:12:09 +0700

    Fix UI + validation login & regis

--- lib/widget/input_field.dart
+++ lib/widget/input_field.dart
@@ -13,6 +13,7 @@ class InputField extends StatelessWidget {
   final bool obsecureText;
   final Function()? onTap;
   final Function(String)? onChanged;
+  final String? Function(String?)? validation;
 
   const InputField(
       {super.key,
@@ -24,7 +25,8 @@ class InputField extends StatelessWidget {
       this.isPassword = false,
       this.obsecureText = false,
       this.onTap,
-      this.onChanged});
+      this.onChanged,
+      this.validation});
 
   @override
   Widget build(BuildContext context) {
@@ -63,24 +65,39 @@ class InputField extends StatelessWidget {
                         const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        width: 1.5,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
                       borderSide: BorderSide(
-                          width: 1.5,
                           color: error == false
                               ? typography300.withOpacity(0.5)
-                              : danger500),
+                              : danger500,
+                          width: 1.5),
+                      borderRadius: BorderRadius.circular(12),
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(12),
                       borderSide: BorderSide(color: primary500, width: 1.5),
-                    )),
+                    ),
+                    errorStyle: regulerReguler.copyWith(color: danger500),
+                    errorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: danger500, width: 1.5))
+                    ),
                 controller: controller,
                 obscureText: obsecureText,
+                validator: validation,
               ),
+              const SizedBox(height: 8,),
               error == true
-                  ? Text(
-                      textError!,
-                      style: regulerReguler.copyWith(color: danger500),
-                    )
+                  ? Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Text(
+                        textError!,
+                        style: regulerReguler.copyWith(color: danger500),
+                      ),
+                  )
                   : Container(),
             ],
           ),
